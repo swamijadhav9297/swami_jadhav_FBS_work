34c6b6f6cb0dc38e972b2cf3707bc21db7cd41ad
swamijadhav9297 <swamijadhav925@gmail.com>
2026-01-03 00:26:38 +0530

    Add files via upload

---
+++ C_PROGRAMMING_MODULE/ASSIG0/assgn0q2.c
@@ -0,0 +1,10 @@
+//2. Write a C program to find the area of a circle.
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+ int a,area;
+ a=5;
+ area=3.14*5*5;
+ printf("the area is %d", area);
+}
---
+++ C_PROGRAMMING_MODULE/ASSIG0/assgn0q4.cpp
@@ -0,0 +1,14 @@
+//4. Write a C program to swap two numbers using a temporary third variable.
+#include<stdio.h>
+int main()
+{
+	int a, b,temp;
+	
+	a=10;
+	b=20;
+	
+	temp=a;
+	a=b;
+	b=temp;
+	
+}
